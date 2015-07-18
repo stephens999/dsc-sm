e61a8da1e31090e0a4ad5d0b20869ecfa7d0d25f
stephens999 <stephens999@gmail.com>
2015-07-17 20:54:38 -0500

    first running prototype, to compute single score

---
+++ one_sample_location/R/methods/median.R
@@ -0,0 +1,11 @@
+# inputs numbers from datafile (an RDS file) and 
+# outputs their median to outfile (RDS file) 
+# usage is Rscript datafile outfile 
+
+args = commandArgs(trailingOnly=TRUE)
+datafile = args[1]
+outfile = args[2]
+
+x=readRDS(datafile)
+saveRDS(median(x),outfile)
+
